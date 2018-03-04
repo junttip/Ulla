5f18d9a320b38e9e58ead435eb99a3f390c34bbf
Paula <paula.k.juntti@gmail.com>
2018-03-04 14:21:48 +0200

    menemene

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Telegrambot provaiders
+        Telegram\Bot\Laravel\TelegramServiceProvider::class,
+
     ],
 
     /*
